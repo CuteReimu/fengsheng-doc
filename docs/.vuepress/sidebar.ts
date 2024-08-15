423a606bd85f9f6b19a48c55e24a3c253d8ca789
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-08-15 17:24:33 +0800

    sidebar不折叠

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -4,28 +4,24 @@ export default sidebar([
   {
     text: '新手入门',
     icon: 'lightbulb',
-    collapsible: true,
     prefix: '/welcome',
     children: ['welcome.md', 'video_introduction.md', 'score_and_season.md', 'welcome_simplified.md'],
   },
   {
     text: '角色技能',
     icon: 'address-book',
-    collapsible: true,
     prefix: '/skills',
     children: ['base.md', 'extend1.md', 'extend2.md'],
   },
   {
     text: '官方规则',
     icon: 'book',
-    collapsible: true,
     prefix: '/guide',
     children: ['how_to_play.md', 'announcement.md', 'faq.md', 'advanced.md'],
   },
   {
     text: '个人攻略',
     icon: 'lightbulb',
-    collapsible: true,
     prefix: '/strategy',
     children: ['how_to_select_role.md', 'tech.md', 'simplified_rules.md'],
   },
