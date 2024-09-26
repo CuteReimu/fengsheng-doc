223a40228b9dd1cf478398319ad825fa2752012f
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-09-26 15:51:17 +0800

    修复链接错误

--- docs/.vuepress/components/MaskIcon.vue
+++ docs/.vuepress/components/MaskIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link to="/guide/how_to_play.html#三、分发角色牌">
+  <router-link to="/document/guide/how_to_play.html#三、分发角色牌">
     <img src="/images/mask.svg" style="transform: scaleY(-1);" width="20" height="20" alt="隐藏角色"/>
   </router-link>
 </template>
