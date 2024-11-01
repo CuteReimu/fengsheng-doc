b555b9cdef8784be372a986460db202ba049c5bf
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-11-01 11:10:39 +0800

    优化搜索玩家页面

--- docs/.vuepress/components/SearchPlayer.vue
+++ docs/.vuepress/components/SearchPlayer.vue
@@ -1,7 +1,7 @@
 <template>
   <el-row>
     <div style="display: flex">
-    <el-autocomplete :fetch-suggestions="getHistory()" v-model="name" placeholder="用户名" :debounce="0" @select="onClickRequest" @focus="getData"></el-autocomplete>
+    <el-autocomplete :fetch-suggestions="getHistory()" v-model="name" placeholder="用户名" :debounce="0" @select="onClickRequest"></el-autocomplete>
     <el-button @click="onClickRequest" :disabled="!name">查询</el-button></div>
   </el-row>
   <el-row>
