000620e2dc6d32f1f36cd439af6a914201e0ffb4
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-11-05 18:14:19 +0800

    侧边栏调整成可折叠

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -1,43 +1,7 @@
 import {sidebar} from "vuepress-theme-hope";
 
 export default sidebar({
-  '/document/': [
-    {
-      text: "新手入门",
-      icon: "lightbulb",
-      link: "/document/welcome/",
-      prefix: "welcome",
-      children: "structure",
-    },
-    {
-      text: "卡牌与身份",
-      icon: "lightbulb",
-      link: "/document/card/",
-      prefix: "card",
-      children: "structure",
-    },
-    {
-      text: "角色技能",
-      icon: "address-book",
-      link: "/document/skills/",
-      prefix: "skills",
-      children: "structure",
-    },
-    {
-      text: "官方规则",
-      icon: "book",
-      link: "/document/guide/",
-      prefix: "guide",
-      children: "structure",
-    },
-    {
-      text: "个人攻略",
-      icon: "lightbulb",
-      link: "/document/strategy/",
-      prefix: "strategy",
-      children: "structure",
-    },
-  ],
+  '/document/': 'structure',
   '/develop/': 'structure',
   '/game_status': [
     {
