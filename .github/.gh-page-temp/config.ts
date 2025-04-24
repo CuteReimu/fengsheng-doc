72b6a265dd224d8db6d94d28bfba92b7c7683dfe
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-04-25 01:11:16 +0800

    不使用cdn

--- .github/.gh-page-temp/config.ts
+++ .github/.gh-page-temp/config.ts
@@ -1,7 +1,6 @@
 import theme from "./theme.js";
 import { defineUserConfig } from 'vuepress';
 import { getDirname, path } from "vuepress/utils";
-import { viteBundler } from '@vuepress/bundler-vite';
 
 const __dirname = getDirname(import.meta.url);
 
@@ -15,50 +14,4 @@ export default defineUserConfig({
 
   title: '《风声·谍战篇》FAQ',
   theme,
-
-  head: [
-    [
-      "script",
-      {
-        src: "//unpkg.com/vue@3.5.13/dist/vue.global.prod.js",
-        async: "false",
-      },
-    ],
-    [
-      "script",
-      {
-        src: "//unpkg.com/vue-router@4.5.0/dist/vue-router.global.prod.js",
-        defer: true,
-      },
-    ],
-    [
-      "script",
-      {
-        src: "https://unpkg.com/chart.js@4.4.9/dist/chart.umd.js",
-        async: "false",
-      },
-    ],
-    [
-      "script",
-      {
-        src: "https://unpkg.com/chartjs-plugin-datalabels@2.2.0/dist/chartjs-plugin-datalabels.js",
-        defer: true,
-      },
-    ],
-  ],
-
-  bundler: viteBundler({
-    viteOptions: {
-      worker: {
-        rollupOptions: {
-          external: [
-            "vue",
-            "vue-router",
-            "chart.js",
-            "chartjs-plugin-datalabels",
-          ],
-        },
-      },
-    }
-  }),
 });
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,6 @@
 import theme from "./theme.js";
 import { defineUserConfig } from 'vuepress';
 import { getDirname, path } from "vuepress/utils";
-import { viteBundler } from '@vuepress/bundler-vite';
 
 const __dirname = getDirname(import.meta.url);
 
@@ -22,79 +21,4 @@ export default defineUserConfig({
 
   title: '《风声·谍战篇》FAQ',
   theme,
-
-  head: [
-    [
-      "script",
-      {
-        src: "https://unpkg.com/vue@3.5.13/dist/vue.global.prod.js",
-        async: "false",
-      },
-    ],
-    [
-      "script",
-      {
-        src: "https://unpkg.com/vue-router@4.5.0/dist/vue-router.global.js",
-        defer: true,
-      },
-    ],
-    [
-      "link",
-      {
-        rel: "stylesheet",
-        href: "https://unpkg.com/element-plus@2.9.8/dist/index.css",
-      },
-    ],
-    [
-      "script",
-      {
-        src: "https://unpkg.com/element-plus@2.9.8/dist/index.full.js",
-      },
-    ],
-    [
-      "script",
-      {
-        src: "https://unpkg.com/chart.js@4.4.9/dist/chart.umd.js",
-        async: "false",
-      },
-    ],
-    [
-      "script",
-      {
-        src: "https://unpkg.com/chartjs-plugin-datalabels@2.2.0/dist/chartjs-plugin-datalabels.js",
-        defer: true,
-      },
-    ],
-    [
-      "script",
-      {
-        src: "https://unpkg.com/chartjs-plugin-annotation@3.1.0/dist/chartjs-plugin-annotation.min.js",
-        defer: true,
-      },
-    ],
-    [
-      "script",
-      {
-        src: "https://unpkg.com/axios@1.8.4/dist/axios.min.js",
-      },
-    ],
-  ],
-
-  bundler: viteBundler({
-    viteOptions: {
-      worker: {
-        rollupOptions: {
-          external: [
-            "vue",
-            "vue-router",
-            "axios",
-            "element-plus",
-            "chart.js",
-            "chartjs-plugin-datalabels",
-            "chartjs-plugin-annotation",
-          ],
-        },
-      },
-    }
-  }),
 });
