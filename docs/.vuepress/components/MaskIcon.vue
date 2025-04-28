7f54ca81e5932b8e9207e5854d9c164e78b974e3
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-04-29 01:00:11 +0800

    修复maskIcon的错误
    (cherry picked from commit ebdc2c7311ee2e78352f0d1484727c309310ed4c)

--- docs/.vuepress/components/MaskIcon.vue
+++ docs/.vuepress/components/MaskIcon.vue
@@ -1,7 +1,5 @@
 <template>
-  <router-link to="/document/guide/how_to_play.html#三、分发角色牌">
-    <img src="/images/mask.svg" style="transform: scaleY(-1);" width="20" height="20" alt="隐藏角色"/>
-  </router-link>
+  <img src="/images/mask.svg" style="transform: scaleY(-1);" width="20" height="20" alt="隐藏角色"/>
 </template>
 
 <script setup>
