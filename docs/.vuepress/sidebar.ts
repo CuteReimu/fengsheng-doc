d115d28bf02a00b4846404b9ea2458d9c54c775a
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-05-09 20:52:14 +0800

    更新页面

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -1,8 +1,25 @@
 import {sidebar} from "vuepress-theme-hope";
 
 export default sidebar({
-  '/document/': 'structure',
-  '/develop/': 'structure',
-  '/game/': 'structure',
+  '/document/': [
+    {
+      text: '卡牌与身份',
+      icon: 'lightbulb',
+      prefix: 'card/',
+      children: 'structure',
+    },
+    {
+      text: '角色列表',
+      icon: 'address-book',
+      prefix: 'skills/',
+      children: 'structure',
+    },
+    {
+      text: '玩家攻略',
+      icon: 'lightbulb',
+      prefix: 'strategy/',
+      children: 'structure',
+    }
+  ],
   '/': false,
 });
--- docs/document/README.md
+++ docs/document/README.md
@@ -2,10 +2,18 @@
 title: 《风声》桌游规则整理
 icon: book
 index: false
-lastUpdated: false
-contributors: false
-pageInfo: false
+pageInfo: [ "Author", "PageView", "Date" ]
 sidebar: false
 ---
+
+::: tip 提示
+
+- 关于卡牌相关的问题，你可以前往[卡牌列表](card/card.md)
+- 关于**神秘人**胜利条件相关的问题，你可以前往[神秘人列表](card/secret_task.md)
+- 关于角色技能相关的问题，你可以前往[角色列表](skills/)
+
+你也可以使用右上角的搜索功能在全部页面进行搜索。
+
+:::
 
 <Catalog></Catalog>
